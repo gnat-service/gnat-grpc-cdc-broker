713021ff5326f8e863bad3f6c1e1c5fadf99b2d3
Chen Wen <joc98@163.com>
2018-05-22 17:13:20 +0800

    Make server to be stoppable

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const configs = require('./configs');
 module.exports = {
     config: app.config,
     start (...args) {
-        app.listen(configs.port, ...args);
+        return app.listen(configs.port, ...args);
     }
 };
